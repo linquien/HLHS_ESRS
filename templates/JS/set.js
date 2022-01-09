69cea511569a2ec1f5b704e0878d12986b1c2b7f
Sunnus <83101926+u811005@users.noreply.github.com>
2022-01-09 22:56:07 +0800

    checkbox_ver

--- templates/JS/set.js
+++ templates/JS/set.js
@@ -1,4 +1,4 @@
-var radios = document.querySelectorAll('input[type=radio]');
+var radios = document.querySelectorAll('input[type=checkbox]');
 function changeHandler(event) {
     if ( this.value === '0' ) {
         var chang = this.parentElement.parentElement;
@@ -10,7 +10,12 @@ function changeHandler(event) {
     else if ( this.value === '2' ) {
         var chang = this.parentElement.parentElement;
         chang.className = 'idk';
-     }  
+     }
+    // for checkbox
+    else if ( this.value == 'on' ) {
+        var chang = this.parentElement.parentElement;
+        chang.className = 'idk';
+     }   
  }
  
  Array.prototype.forEach.call(radios, function(radio) {
